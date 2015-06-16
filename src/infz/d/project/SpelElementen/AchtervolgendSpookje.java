211ff3dbb074c732e1c7b0950d1e8412f98944b9
MrRichTee <ostmyst@gmail.com>
2015-06-16 22:31:32 +0200

    AchtervolgendSpookje

--- src/infz/d/project/SpelElementen/AchtervolgendSpookje.java
+++ src/infz/d/project/SpelElementen/AchtervolgendSpookje.java
@@ -10,8 +10,12 @@ import infz.d.project.Enums.Richting;
 
 import infz.d.project.GUI.Vakje;
 import infz.d.project.Interfaces.AchtervolgenBewegenAlgoritme;
+import infz.d.project.Interfaces.AchtervolgenBewegen;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -19,11 +23,10 @@ import java.util.Stack;
  *
  * @author Method
  */
-public class AchtervolgendSpookje extends Spookje implements AchtervolgenBewegenAlgoritme {
-
-    Stack<Vakje> openStack = new Stack<>();
-    Stack<Vakje> closedStack = new Stack<>();
-
+public class AchtervolgendSpookje extends Spookje {
+    private Vakje pacmanRichting;
+    private AchtervolgenBewegen bewegen = new AchtervolgenBewegen();
+    
     public AchtervolgendSpookje(Vakje vakje, String naam) {
         this.vakje = vakje;
         this.startPositie = vakje;
@@ -40,57 +43,110 @@ public class AchtervolgendSpookje extends Spookje implements AchtervolgenBewegen
 
     @Override
     public void bewegen() {
+        if(this.vakje == null) // moet nooit kunnen
+            return;
+        
+        if(this.vakje.getPacman() != null)
+            return;
+        
+        checkPacman();
+        
+        Iterator iter = this.vakje.getBuurLijst().entrySet().iterator();
 
+        while (iter.hasNext()) {
+            Map.Entry pair = (Map.Entry) iter.next();
+            if (pair.getValue() == bewegen.geefCell(this.vakje)) {
+                switch(pair.getKey().toString()){
+                    case "NOORD":
+                        if (vakje.getBuurLijst().containsKey(Richting.NOORD)) {
+                            Vakje vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.NOORD);
+                            bewegenSetters(Richting.NOORD);
+                        }
+                        break;
+                    case "OOST":
+                        if (vakje.getBuurLijst().containsKey(Richting.OOST)) {
+                            Vakje vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.OOST);
+                            bewegenSetters(Richting.OOST);
+                        }
+                        break;
+                    case "ZUID":
+                        if (vakje.getBuurLijst().containsKey(Richting.ZUID)) {
+                            Vakje vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.ZUID);
+                            bewegenSetters(Richting.ZUID);
+                        }
+                        break;
+                    case "WEST":
+                        if (vakje.getBuurLijst().containsKey(Richting.WEST)) {
+                            Vakje vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.WEST);
+                            bewegenSetters(Richting.WEST);
+                        }
+                        break;
+                    default:
+                        System.out.println("AchtervolgendSpookje - bewegen: " + pair.getKey().toString() + " niet gevonden!");
+                        break;
+                }
+            }
+        }
+        
+        checkPacman();
+        
+        this.vakje.getSpelbord().tekenOpnieuw();
     }
 
+    private void bewegenSetters(Richting richting) {
+        Vakje vakje = null;
+        this.vakje.setSpookjeNull(this);
+        
+        switch (richting.toString()) {
+            case "NOORD":
+                if (naam.equals("pinky"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.PINKYNOORD);
+                
+                if (naam.equals("clyde"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.CLYDENOORD);
+                
+                vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.NOORD);
+                break;
+            case "OOST":
+                if (naam.equals("pinky")) 
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.PINKYOOST);
+                
+                if (naam.equals("clyde"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.CLYDEOOST);
+                
+                vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.OOST);
+                break;
+            case "ZUID":
+                if (naam.equals("pinky"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.PINKYZUID);
+                
+                if (naam.equals("clyde"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.CLYDEZUID);
+               
+                vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.ZUID);
+                break;
+            case "WEST":
+                if (naam.equals("pinky"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.PINKYWEST);
+               
+                if (naam.equals("clyde"))
+                    this.image = this.vakje.getImageLoader().selectSpookjeAfbeelding(Afbeelding.Spookje.CLYDEWEST);
+                
+                vakje = (Vakje) this.vakje.getBuurLijst().get(Richting.WEST);
+                break;
+            default:
+                System.out.println("AchtervolgendSpookje - " + this.naam + " - bewegenSetters: " + richting.toString() + " niet gevonden!" );
+                break;
+        }
+        vakje.setSpookje(this);
+        this.setVakje(vakje);
+    }
+    
     @Override
     public void draw(Graphics g) {
         this.row = vakje.getXPositie();
         this.column = vakje.getYPositie();
 
         g.drawImage(this.image, column * CELL, row * CELL, 35, 35, null);
-    }
-
-    @Override
-    public boolean bfs() {
-        /*
-         int r = 0, c = 0;
-         int startXPos = this.vakje.getXPositie(), startYPos = this.vakje.getYPositie();
-         int[] dx = {1, -1, 0, 0};//right, left, NA, NA
-         int[] dy = {0, 0, 1, -1};//NA, NA, bottom, top
-         Vakje[][] grid = new Vakje[r][c];//Main grid
-         if (this.vakje.getPacman() != null) {
-         return true;//He's already there
-         } else {
-         grid[finXPos][finYPos] = 'G';//finish
-         Queue<int[]> q = new LinkedList<int[]>();
-         int[] start = {startXPos, startYPos};// start coordinaten
-         q.add(start);//Adding start to the queue since we're already visiting it
-         grid[startXPos][startYPos] = 'B';
-         while (q.peek() != null) {
-         int[] curr = q.poll();//poll or remove. Same thing
-         for (int i = 0; i < 4; i++)//for each direction
-         {
-         if ((curr[0] + dx[i] >= 0 && curr[0] + dx[i] < r) && (curr[1] + dy[i] >= 0 && curr[1] + dy[i] < c)) {
-         //Checked if x and y are correct. ALL IN 1 GO
-         int xc = curr[0] + dx[i];//Setting current x coordinate
-         int yc = curr[1] + dy[i];//Setting current y coordinate
-         if (grid[xc][yc] == 'G')//Destination found
-         {
-         //System.out.println(xc+" "+yc);
-         return true;
-         } else if (grid[xc][yc] == 'E')//Movable. Can't return here again so setting it to 'B' now
-         {
-         //System.out.println(xc+" "+yc);
-         grid[xc][yc] = 'B';//now BLOCKED
-         int[] temp = {xc, yc};
-         q.add(temp);//Adding current coordinates to the queue
-         }
-         }
-         }
-         }
-         return false;//Will return false if no route possible
-         }*/
-        return true;
     }
 }
