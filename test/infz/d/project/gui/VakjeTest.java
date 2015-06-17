1bece0d3dfac999508d414e8111fccbb2642498d
sebastiaans10 <sebastiaans10@gmail.com>
2015-06-17 13:55:12 +0200

    unitTest bijgewerkt

--- test/infz/d/project/gui/VakjeTest.java
+++ test/infz/d/project/gui/VakjeTest.java
@@ -6,9 +6,12 @@
 package infz.d.project.gui;
 
 import infz.d.project.GUI.Vakje;
+import infz.d.project.Ondersteunend.ImageLoader;
+import infz.d.project.SpelElementen.AchtervolgendSpookje;
 import infz.d.project.SpelElementen.Pacman;
 import infz.d.project.SpelElementen.SpelElement;
 import infz.d.project.SpelElementen.Spookje;
+import infz.d.project.SpelElementen.WillekeurigSpookje;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import org.junit.After;
@@ -24,100 +27,158 @@ import static org.junit.Assert.*;
  */
 public class VakjeTest {
 
+    ImageLoader imageLoader;
+    Pacman pacman;
+    Vakje vakje;
+
     public VakjeTest() {
+    }
+
+    private void maakObjectenLeeg() {
+        imageLoader = null;
+        pacman = null;
+        vakje = null;
     }
 
 //Testgevallen testmaat1:
     /**
-     * Geval1
+     * Geval1 //
      */
-    @Test
-    public void testPuntenOptellenVanBuurVakje() {
-        int punten = 0;
-        int expResult = 800;
-        Vakje vakje = new Vakje(2, 3, "inky", null);
-        Vakje instance = vakje;
-        int result = instance.puntenOptellenVanBuurVakje(vakje);
-        punten = punten + result;
-        vakje = null;
-        vakje = new Vakje(2, 3, "blinky", null);
-        int resultN = instance.puntenOptellenVanBuurVakje(vakje);
-        punten = punten + resultN;
-        vakje = null;
-        vakje = new Vakje(2, 3, "pinky", null);
-        int resultN2 = instance.puntenOptellenVanBuurVakje(vakje);
-        punten = punten + resultN2;
-        vakje = null;
-        vakje = new Vakje(2, 3, "clyde", null);
-        int resultN3 = instance.puntenOptellenVanBuurVakje(vakje);
-        punten = punten + resultN3;
-        
-        assertEquals(expResult, punten);
-    }
-
-    /**
-     * Geval2
-//     */
 //    @Test
-//    public void testPuntenOptellenVanBuurVakje() {
-//        Vakje vakje = new Vakje(2, 3, "pad", null);
-//        Vakje instance = vakje;
-//        int expResult = 0;
-//        int result = instance.puntenOptellenVanBuurVakje(vakje);
-//        assertEquals(expResult, result);
-//
-//    }
-//
-    /**
-     * Geval3
-//     */
-//    @Test
-//    public void testPuntenOptellenVanBuurVakje() {
-//        Vakje vakje = new Vakje(2, 3, "bolletje", null);
-//        Vakje instance = vakje;
-//        int expResult = 10;
-//        int result = instance.puntenOptellenVanBuurVakje(vakje);
-//        assertEquals(expResult, result);
-//
-//    }
-
-    //Testgevallen testmaat2:
-    /**
-//     * Geval1
-////     */
-//  @Test
 //    public void testPuntenOptellenVanBuurVakje() {
 //        int punten = 0;
 //        int expResult = 800;
-//        Vakje vakje = new Vakje(2, 3, "inky", null);
-//        Vakje instance = vakje;
-//        int result = instance.puntenOptellenVanBuurVakje(vakje);
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "inky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int result = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + result;
-//        vakje = null;
-//        vakje = new Vakje(2, 3, "blinky", null);
-//        int resultN = instance.puntenOptellenVanBuurVakje(vakje);
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "blinky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + resultN;
-//        vakje = null;
-//        vakje = new Vakje(2, 3, "pinky", null);
-//        int resultN2 = instance.puntenOptellenVanBuurVakje(vakje);
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "pinky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN2 = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + resultN2;
-//        vakje = null;
-//        vakje = new Vakje(2, 3, "clyde", null);
-//        int resultN3 = instance.puntenOptellenVanBuurVakje(vakje);
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "clyde", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN3 = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + resultN3;
+//     maakObjectenLeeg();
 //        
 //        assertEquals(expResult, punten);
 //    }
-
+//    /**
+//     * Geval2
+//     */
+//    @Test
+//    public void testPuntenOptellenVanBuurVakje() {
+//        int expResult = 0;
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "pad", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int result = vakje.puntenOptellenVanVakje(vakje);
+//        maakObjectenLeeg();
+//        assertEquals(expResult, result);
+//
+//    }
+//
+//    /**
+//     * Geval3 //
+//     */
+//    @Test
+//    public void testPuntenOptellenVanBuurVakje() {
+//        int expResult = 10;
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "bolletje", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//
+//        int result = vakje.puntenOptellenVanVakje(vakje);
+//        maakObjectenLeeg();
+//        assertEquals(expResult, result);
+//
+//    }
+    //Testgevallen testmaat2:
+    /**
+     * // * Geval1 ////
+     */
+//    @Test
+//    public void testPuntenOptellenVanBuurVakje() {
+//        int punten = 0;
+//        int expResult = 800;
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "inky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int result = vakje.puntenOptellenVanVakje(vakje);
+//        punten = punten + result;
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "blinky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN = vakje.puntenOptellenVanVakje(vakje);
+//        punten = punten + resultN;
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "pinky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN2 = vakje.puntenOptellenVanVakje(vakje);
+//        punten = punten + resultN2;
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "clyde", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN3 = vakje.puntenOptellenVanVakje(vakje);
+//        punten = punten + resultN3;
+//          maakObjectenLeeg();
+//
+//        assertEquals(expResult, punten);
+//    }
 //    /**
 //     * Geval2
 ////     */
 //    @Test
 //    public void testPuntenOptellenVanBuurVakje() {
-//        Vakje vakje = new Vakje(2, 3, "pad", null);
-//        Vakje instance = vakje;
 //        int expResult = 0;
-//        int result = instance.puntenOptellenVanBuurVakje(vakje);
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "pad", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int result = vakje.puntenOptellenVanVakje(vakje);
+//          maakObjectenLeeg();
 //        assertEquals(expResult, result);
 //
 //    }
@@ -127,33 +188,51 @@ public class VakjeTest {
 //     */
 //    @Test
 //    public void testPuntenOptellenVanBuurVakje() {
-//        Vakje vakje = new Vakje(2, 3, "blinky", null);
-//        Vakje instance = vakje;
 //        int expResult = 200;
-//        int result = instance.puntenOptellenVanBuurVakje(vakje);
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "blinky", null, imageLoader);
+//        Vakje instance = vakje;
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int result = instance.puntenOptellenVanVakje(vakje);
+//        maakObjectenLeeg();
 //        assertEquals(expResult, result);
-//
 //    }
-
 //    /**
 //     * Geval4
 //     */
 //    @Test
 //    public void testPuntenOptellenVanBuurVakje() {
-//       int punten = 0;
+//        int punten = 0;
 //        int expResult = 410;
-//        Vakje vakje = new Vakje(2, 3, "inky", null);
-//        Vakje instance = vakje;
-//        int result = instance.puntenOptellenVanBuurVakje(vakje);
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "inky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int result = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + result;
-//        vakje = null;
-//        vakje = new Vakje(2, 3, "pinky", null);
-//        int resultN = instance.puntenOptellenVanBuurVakje(vakje);
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "pinky", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + resultN;
-//        vakje = null;
-//        vakje = new Vakje(2, 3, "bolletje", null);
-//        int resultN2 = instance.puntenOptellenVanBuurVakje(vakje);
+//        maakObjectenLeeg();
+//
+//        imageLoader = new ImageLoader();
+//        vakje = new Vakje(2, 3, "bolletje", null, imageLoader);
+//        pacman = new Pacman(vakje);
+//        vakje.setPacman(pacman);
+//        pacman.setOnverslaanbaar(true);
+//        int resultN2 = vakje.puntenOptellenVanVakje(vakje);
 //        punten = punten + resultN2;
+//
+//        maakObjectenLeeg();
 //        assertEquals(expResult, punten);
 //
 //    }
